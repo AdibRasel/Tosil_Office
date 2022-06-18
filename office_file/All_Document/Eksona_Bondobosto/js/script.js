b1efd8b73bd7fd8c4d810cefc6bfb4dd3443d6ba
AdibRasel <97701792+AdibRasel@users.noreply.github.com>
2022-06-19 01:31:44 +0600

    print page setup
    print page setup

--- office_file/All_Document/Eksona_Bondobosto/js/script.js
+++ office_file/All_Document/Eksona_Bondobosto/js/script.js
@@ -39,9 +39,67 @@ function submit1(){
 
 
 // print button 
-function printa(){
-    var print_div = document.getElementById("print_box");
-    print();
+// function printa(){
+//     var print_div = document.getElementById("print_box");
+//     print();
     
-}
+// }
 
+
+// var divName = document.getElementById("print_box")
+
+// function printDiv(elem)
+// {
+//     var mywindow = window.open();
+//     var content = document.getElementById(elem).innerHTML;
+//     var realContent = document.body.innerHTML;
+//     mywindow.document.write(content);
+
+ 
+//     mywindow.document.write('<link rel="stylesheet" href="css/style.css">');
+//     mywindow.document.write('</head><body >');
+//     mywindow.document.write(data);
+//     mywindow.document.write('</body></html>');
+
+
+//     mywindow.document.close(); // necessary for IE >= 10
+//     mywindow.focus(); // necessary for IE >= 10*/
+//     mywindow.print();
+//     document.body.innerHTML = realContent;
+//     mywindow.close();
+//     return true;
+// }
+
+
+
+// function printThisDiv(elem) {
+//     Popup($(elem).html());
+// }
+
+// function Popup(data) {
+//     var mywindow = window.open('', 'new div', 'height=400,width=600');
+//     mywindow.document.write('<html><head><title></title>');
+//     mywindow.document.write('<link rel="stylesheet" href="css/midday_receipt.css" type="text/css" />');
+//     mywindow.document.write('</head><body >');
+//     mywindow.document.write(data);
+//     mywindow.document.write('</body></html>');
+
+//     mywindow.print();
+//     mywindow.close();
+
+//     return true;
+// }
+
+// print function
+function printDiv(divName){
+    var printContents = document.getElementById(divName).innerHTML;
+    var originalContents = document.body.innerHTML;
+
+    document.body.innerHTML = printContents;
+
+    window.print();
+
+    document.body.innerHTML = originalContents;
+
+}
+// end print function
